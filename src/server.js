aca40cb7e327590ad073395d29e02d369e8bec68
Nicolas <nicolas.kraide@gamil.com>
2025-01-24 12:20:46 -0300

    install nodemon

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+app.get("/message/:id", (request, response) => {
+
+});
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
