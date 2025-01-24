f91ea0dccb71d02aedb7598d4a26590c0567a44c
Nicolas <nicolas.kraide@gamil.com>
2025-01-24 12:35:24 -0300

    route and query params

--- src/server.js
+++ src/server.js
@@ -2,9 +2,18 @@ const express = require("express");
 
 const app = express();
 
-app.get("/message/:id", (request, response) => {
+app.get("/message/:id/:user", (request, response) => {
+  const {id, user} = request.params;
 
+  response.send(`user name ${user},and id ${id}`);
+});
+
+app.get("/users", (request, response) => {
+  const {page, limit} = request.query;
+
+  response.send(`Message at page: ${page} and limit: ${limit}`);
 });
 
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
+
