e479582d7454dcfd758e4ba90e5f682db1821d68
Nicolas <nicolas.kraide@gamil.com>
2025-01-27 18:45:17 -0300

    AppError

--- src/server.js
+++ src/server.js
@@ -1,13 +1,11 @@
-const { application } = require("express");
-const express = require("express");
+const express = require('express')
 
-const routes = require("./routes");
+const routes = require('./routes')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-app.use(routes);
+app.use(routes)
 
-const PORT = 3333;
-app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
-
+const PORT = 3333
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
