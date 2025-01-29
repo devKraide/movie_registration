b89046cdc7b312e8a3100479ca1ed194ec701286
Nicolas <nicolas.kraide@gamil.com>
2025-01-29 10:24:22 -0300

    encrypting passwords

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,11 +1,11 @@
+const {hash} = require('bcryptjs');
 const AppError = require('../utils/AppError')
-
 const sqliteConnection = require("../database/sqlite");
 
 class UsersController {
   async create(request, response) {
     const { name, email, password } = request.body;
-    
+
     const database = await sqliteConnection(); 
 
     const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
@@ -14,10 +14,14 @@ class UsersController {
       throw new AppError("Email address already used.")
     }
 
-    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, password]);
+    const passwordHash = await hash(password, 8);
+
+    await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, passwordHash]);
 
     return response.status(201).json();
   }
 }
+
+
 
 module.exports = UsersController
