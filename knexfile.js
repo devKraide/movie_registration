050a81c6c03aa0987b135005693bc74142d4b37e
Nicolas <nicolas.kraide@gamil.com>
2025-01-30 08:43:25 -0300

    enabling cascade exclusion

--- knexfile.js
+++ knexfile.js
@@ -8,6 +8,8 @@ module.exports = {
     connection: {
       filename: path.resolve(__dirname, 'src', 'database', 'database.db')
     },
+    pool: { 
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb)}, // enable the foreign key = enable the exclusion in cascades
     migrations: {
       directory: path.resolve(__dirname, 'src', 'database', 'knex', 'migrations')
     },
