ddde4c77a0c9abd0d33cfe471b4a4a9cf3baa203
Nicolas <nicolas.kraide@gamil.com>
2025-01-30 08:12:12 -0300

    knexfile bug fix

--- src/database/knex/migrations/20250130103442_createNotes.js
+++ src/database/knex/migrations/20250130103442_createNotes.js
@@ -1,15 +1,12 @@
-exports.up = knex => knex.schema.createTable('notes', table => { 
-  table.increments('id');
-  table.string('title');
-  table.text('description');
-  table.integer('rating');
+exports.up = knex => knex.schema.createTable("notes", table => {
+  table.increments("id");
+  table.text("title");
+  table.text("description");
+  table.integer("rating");
+  table.integer("user_id").references("id").inTable("users")
 
-  table.integer("user_id").references("id").inTable("users").onDelete("CASCADE");
-
-  table.timestamp('created_at').defaultTo(knex.fn.now());
-  table.timestamp('updated_at').defaultTo(knex.fn.now());
-
+  table.timestamp("created_at").default(knex.fn.now())
+  table.timestamp("updated_at").default(knex.fn.now())
 });
 
-
-exports.down = knex => knex.schema.dropTable('notes',);
+exports.down = knex => knex.schema.dropTable("notes");
